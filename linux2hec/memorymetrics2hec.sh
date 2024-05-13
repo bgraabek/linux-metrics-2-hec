453495ba3eaab959448439744fcbe44148e070c5
bgraabek <40198442+bgraabek@users.noreply.github.com>
2024-05-13 16:21:15 +0100

    Update memorymetrics2hec.sh
    Sending all data as one batch event

--- linux2hec/memorymetrics2hec.sh
+++ linux2hec/memorymetrics2hec.sh
@@ -7,6 +7,4 @@
 export S_TIME_FORMAT=ISO
 
 free -m -w | xargs | awk '{print"{\"host\":\"'"$HOST"'\", \"source\":\"linux_rammetrics\", \"fields\":{\"#category\":\"linux_metrics\", \"ram"$1"\": "$9",\"ram"$2"\": "$10",\"ram"$3"\": "$11",\"ram"$4"\": "$12",\"ram"$5"\": "$13",\"ram"$6"\": "$14",\"ram"$7"\": "$15", \"swap"$1"\": "$17",\"swap"$2"\": "$18",\"swap"$3"\": "$19"}}"}' |\
-while read line; do \
-  /usr/bin/curl $HEC_HOST/api/v1/ingest/hec -X POST -H "Content-Type: text/plain; charset=utf-8" -H "Authorization: Bearer $INGEST_TOKEN" --data "$line"; \
-done
+/usr/bin/curl $HEC_HOST/api/v1/ingest/hec -X POST -H "Content-Type: text/plain; charset=utf-8" -H "Authorization: Bearer $INGEST_TOKEN" --data-binary @-
